0d65ed49a87d9a4c3518b943a4c684a638d43050
mkaane <mkaan97@gmail.com>
2022-05-21 00:17:13 +0300

    Homepage and Chatpage are created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Route } from "react-router-dom";
+import Homepage from './Pages/Homepage';
+import ChatPage from './Pages/ChatPage';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Route path="/" component={Homepage} exact/> {/* exact means: if we go chats, it will show us 
+      homepage and chats because chats includes "/" and router will bring us / and /chats*/}
+      <Route path="/chats" component={ChatPage} />
     </div>
   );
 }
